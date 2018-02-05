6951fff1b0290ddbdb7efca9928a2e3390963322
siddiqabu <35294835+siddiqabu@users.noreply.github.com>
2018-02-05 15:57:03 +0530

    long.c

---
+++ long.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int base,exponent;
+long long result =1;
+printf("enter the base number");
+scanf("%d",&base);
+printf("enter the exponent");
+scanf("%d",&exponent);
+while(exponent ! =0)
+printf("the result is %d",result);
+return 0;
+}
